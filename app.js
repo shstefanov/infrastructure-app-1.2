b7a5876e9264a7171fee8d6a05973b9e910d81d8
Stefan Stefanov <s.h.stefanov@mail.bg>
2016-02-03 14:43:25 +0200

    set app mode

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var infrastructure = require("infrastructure");
-infrastructure({}, function(err, env){
+infrastructure({ mode: "development" }, function(err, env){
   if(err) console.error(err);
   console.log("Application started");
 });
